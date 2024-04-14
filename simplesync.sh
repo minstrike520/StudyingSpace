ba65231a0a6f6952b6629bf1299823b19e8ddc37
minstrike520 <minstrike520@gmail.com>
2024-04-14 12:09:05 +0800

    phone update Sun Apr 14 12:09:05 CST 2024

--- simplesync.sh
+++ simplesync.sh
@@ -3,6 +3,6 @@ MY_DEVICE="$(head -1 ~/THIS_DEVICE)"
 echo "Currently syncing on $MY_DEVICE"
 cd ~/shortcuts/TheVault/
 git add .
-git commit -m "$(MY_DEVICE)_update $(date)"
+git commit -m "$MY_DEVICE update $(date)"
 git pull
 git push
