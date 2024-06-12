fa3c70c1f99e44c0b58d0fe206350d26a3952c78
minstrike520 <minstrike520@gmail.com>
2024-06-12 17:38:14 +0800

    update@MiPad: untitled 2024-06-12(Wed), 17:38

--- simplesync.sh
+++ simplesync.sh
@@ -1,6 +1,7 @@
 set -e
 MY_DEVICE="$(head -1 ~/THIS_DEVICE)"
 echo "Currently syncing on $MY_DEVICE"
+echo -n "Name your update description (press ENTER to use default option): "
 cd ~/shortcuts/TheVault/
 set +e
 
@@ -11,7 +12,7 @@ if [[ $custom_description = "" ]]; then
   custom_description="untitled"
 fi
 
-git commit -m "update@$MY_DEVICE: $(date +"%Y-%m-%d(%a), %H:%M")"
+git commit -m "update@$MY_DEVICE: $custom_description $(date +"%Y-%m-%d(%a), %H:%M")"
 # update@my_device: afternoon T1 (2024-06-12, 17:26)
 
 git pull
