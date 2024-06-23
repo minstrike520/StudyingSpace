78cb75a0786ff8426f29d002cabd141847cd39d9
minstrike520 <minstrike520@gmail.com>
2024-06-23 12:47:33 +0800

    update@MiPad: morning P-4568-2 review#1 2024-06-23(Sun)

---
+++ custom_commit.sh
@@ -0,0 +1,12 @@
+set -e
+MY_DEVICE="$(head -1 ~/THIS_DEVICE)"
+echo "Making commit on $MY_DEVICE"
+echo -n "Name your update description (press ENTER to use default option): "
+cd ~/shortcuts/TheVault/
+
+read custom_description
+if [[ $custom_description = "" ]]; then
+  git commit -m "update@$MY_DEVICE: $(date +"%H:%M %Y-%m-%d(%a)")"
+else
+  git commit -m "update@$MY_DEVICE: $custom_description $(date +"%Y-%m-%d(%a)")"
+fi
--- simplesync.sh
+++ simplesync.sh
@@ -1,19 +1,10 @@
 set -e
-MY_DEVICE="$(head -1 ~/THIS_DEVICE)"
-echo "Currently syncing on $MY_DEVICE"
-echo -n "Name your update description (press ENTER to use default option): "
 cd ~/shortcuts/TheVault/
-set +e
 
 git add .
 
-read custom_description
-if [[ $custom_description = "" ]]; then
-  git commit -m "update@$MY_DEVICE: $(date +"%H:%M %Y-%m-%d(%a)")"
-else
-  git commit -m "update@$MY_DEVICE: $custom_description $(date +"%Y-%m-%d(%a)")"
-fi
-
+/bin/bash ./custom_commit.sh
+set +e
 # update@my_device: afternoon T1 (2024-06-12, 17:26)
 
 git pull
